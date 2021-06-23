3c6b199250df6a0f7a9338ecbece121831519ac0
CodeUnique90 <86324083+CodeUnique90@users.noreply.github.com>
2021-06-23 12:35:22 +0530

    Update pattern14.py

--- pattern14.py
+++ pattern14.py
@@ -7,7 +7,7 @@ Created on Tue Jun 22 20:40:13 2021
 
 for row in range(7):
     for col in range(5):
-        if col==0 or col==4 or(row==3 and(col>0 and col<4)):
+        if ((row==0 or row==6) and col!=2) or col==2:
             print("*" , end="")
         else:
             print(end=" ")
