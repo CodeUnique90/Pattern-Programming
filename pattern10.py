9fbbe99a73df58192328267962598cafa100f716
CodeUnique90 <86324083+CodeUnique90@users.noreply.github.com>
2021-06-23 12:29:50 +0530

    Update pattern10.py

--- pattern10.py
+++ pattern10.py
@@ -8,7 +8,7 @@ Created on Sun Jun 20 13:13:32 2021
 
 for row in range(7):
     for col in range(5):
-        if (col==0) or (col==4 and (row!=0 and row!=6)) or ((row==0 or row==6) and (col>0 and col<4)):
+        if col == 0 or ((row==0 or row==3 or row==6) and col>0):
             print("*" , end="")
         else:
             print(end=" ")
